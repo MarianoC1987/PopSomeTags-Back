5a9507281652dc13626748d2432f08489e551acc
Mariano Camacho <macadema@gmail.com>
2023-05-23 23:57:09 -0300

    agregando endpoints de usuarios y controladores

--- routes/users.js
+++ routes/users.js
@@ -3,10 +3,14 @@ const { runValidation } = require("../validators/runValidation");
 const {
   userValidator,
   loginValidator,
+  verifyToken,
 } = require("../validators/validatorUsers");
 const {
   registerController,
   loginController,
+  searchId,
+  allUsers,
+  currentLoggedUser,
 } = require("../controllers/controllerUsers");
 
 const router = express.Router();
@@ -16,5 +20,14 @@ router.post("/registro", userValidator, runValidation, registerController);
 
 //Login
 router.post("/login", loginValidator, runValidation, loginController);
+
+//Saber info del usuario actualmente logueado
+router.get("/usuarios/currentsession", verifyToken, currentLoggedUser);
+
+//Todos los usuarios
+router.get("/usuarios", allUsers);
+
+//Perfil usuario
+router.get("/usuario/:id", searchId);
 
 module.exports = router;
