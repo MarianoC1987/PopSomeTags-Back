0648d065f979286ce36e275a4b9d4d959c887e26
Mariano Camacho <macadema@gmail.com>
2023-06-09 16:54:56 -0300

    agregando rutas para eliminar usuarios y editar informacion

--- routes/users.js
+++ routes/users.js
@@ -4,6 +4,7 @@ const {
   userValidator,
   loginValidator,
   verifyToken,
+  verifyAdmin,
 } = require("../validators/validatorUsers");
 const {
   registerController,
@@ -11,6 +12,8 @@ const {
   searchId,
   allUsers,
   currentLoggedUser,
+  deleteUser,
+  editUserInfo,
 } = require("../controllers/controllerUsers");
 
 const router = express.Router();
@@ -22,12 +25,18 @@ router.post("/registro", userValidator, runValidation, registerController);
 router.post("/login", loginValidator, runValidation, loginController);
 
 //Saber info del usuario actualmente logueado
-router.get("/usuarios/currentsession", verifyToken, currentLoggedUser);
+router.get("/usuarios/currentsession", currentLoggedUser);
 
 //Todos los usuarios
-router.get("/usuarios", allUsers);
+router.get("/usuarios", verifyToken, allUsers);
 
 //Perfil usuario
-router.get("/usuario/:id", searchId);
+router.get("/usuario/:id", verifyToken, searchId);
+
+//Eliminar usuario
+router.delete("/usuario/borrar/:id", verifyToken, deleteUser);
+
+//Editar info de usuario
+router.put("/usuario/editar/:id", editUserInfo);
 
 module.exports = router;
